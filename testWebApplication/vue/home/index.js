1bca33369981f81aa669aa3c2dd6ab40f89a8fc4
gezb19890407 <837754013@qq.com>
2017-07-17 16:27:07 +0800

    1

---
+++ testWebApplication/vue/home/index.js
@@ -0,0 +1,30 @@
+﻿
+
+Vue.component('todo-item', {
+    props: ['entity'],
+    template: "<li>{{entity.text}}</li>"
+});
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "hello ",
+        a: false,
+        entityList: [
+            { id: 1, text: "第1条" },
+            { id: 2, text: "第2条" }
+        ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('');
+        }
+    },
+    filters: {
+        capitalize: function (value) {
+            if (!value) return "";
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        }
+    }
+});
