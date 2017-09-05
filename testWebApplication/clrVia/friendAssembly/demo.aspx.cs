a3b5c4f1491caacae12dd0e213cf0bab3ba67e8e
gezb19890407 <837754013@qq.com>
2017-09-05 14:06:54 +0800

    1

---
+++ testWebApplication/clrVia/friendAssembly/demo.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Wintellect, PublicKey=12345678...90abcdef")]
+[assembly: InternalsVisibleTo("Microsoft, PublicKey=b77a5c56...1934e089")]
+internal class SomeInternalType
+{
+
+}
+
+namespace testWebApplication.clrVia.friendAssembly
+{
+    public partial class demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SomeInternalType s = new SomeInternalType();
+        }
+    }
+
+}
